fc5e477b6679682d2058a67e01700aa9f6457ea9
Avasara <devloper300@gmail.com>
2024-01-25 19:05:59 +0100

    Added Log.d messages to DiscoveryManagerListener

--- app/src/main/java/com/example/tv_remote_wifi_app/MainActivity.java
+++ app/src/main/java/com/example/tv_remote_wifi_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tv_remote_wifi_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.connectsdk.discovery.DiscoveryManager;
 import com.connectsdk.discovery.DiscoveryManagerListener;
@@ -10,6 +11,8 @@ import com.connectsdk.service.command.ServiceCommandError;
 import com.connectsdk.device.ConnectableDevice;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +27,24 @@ public class MainActivity extends AppCompatActivity {
         discoveryManager.addListener(new DiscoveryManagerListener() {
             @Override
             public void onDeviceAdded(DiscoveryManager manager, ConnectableDevice device) {
-
+                Log.d(TAG, "We found a Device!");
             }
 
             @Override
             public void onDeviceUpdated(DiscoveryManager manager, ConnectableDevice device) {
+                Log.d(TAG, "We made new discoveries!");
 
             }
 
             @Override
             public void onDeviceRemoved(DiscoveryManager manager, ConnectableDevice device) {
+                Log.d(TAG, "A device went Dark!");
 
             }
 
             @Override
             public void onDiscoveryFailed(DiscoveryManager manager, ServiceCommandError error) {
-
+                Log.d(TAG, "We found no devices!");
             }
         });
     }
