2b47d5f62b8b0fc2fd02b3db3727de37fbad90df
Avasara <devloper300@gmail.com>
2024-01-25 19:00:17 +0100

    ConnectSDK set up and working fine
    We got ConnectSDK working.

---
+++ app/src/main/java/com/example/tv_remote_wifi_app/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.tv_remote_wifi_app;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.connectsdk.discovery.DiscoveryManager;
+import com.connectsdk.discovery.DiscoveryManagerListener;
+import com.connectsdk.service.command.ServiceCommandError;
+import com.connectsdk.device.ConnectableDevice;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize DiscoveryManager
+        DiscoveryManager.init(getApplicationContext());
+        DiscoveryManager discoveryManager = DiscoveryManager.getInstance();
+        discoveryManager.start();
+
+        discoveryManager.addListener(new DiscoveryManagerListener() {
+            @Override
+            public void onDeviceAdded(DiscoveryManager manager, ConnectableDevice device) {
+
+            }
+
+            @Override
+            public void onDeviceUpdated(DiscoveryManager manager, ConnectableDevice device) {
+
+            }
+
+            @Override
+            public void onDeviceRemoved(DiscoveryManager manager, ConnectableDevice device) {
+
+            }
+
+            @Override
+            public void onDiscoveryFailed(DiscoveryManager manager, ServiceCommandError error) {
+
+            }
+        });
+    }
+}
