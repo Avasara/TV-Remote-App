f161f28f46417bda0a83098084b2500a2dc17445
Avasara <devloper300@gmail.com>
2024-01-26 00:32:29 +0100

    Discovered Devices
    I've finally gotten it able to discover smart devices on the network. Currently the way it displays them is unacceptable so I'm working on that now and it will be fixed in the next commit.
    
    Most of the unneeded code is gone. Right now its just the bare minimum needed to discover the devices.

--- app/src/main/java/com/example/tv_remote_wifi_app/MainActivity.java
+++ app/src/main/java/com/example/tv_remote_wifi_app/MainActivity.java
@@ -1,65 +1,64 @@
 package com.example.tv_remote_wifi_app;
 
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.connectsdk.device.ConnectableDeviceListener;
-import com.connectsdk.device.DevicePicker;
 import com.connectsdk.discovery.DiscoveryManager;
+import com.connectsdk.discovery.DiscoveryManagerListener;
 import com.connectsdk.device.ConnectableDevice;
-import com.connectsdk.service.DeviceService;
 import com.connectsdk.service.command.ServiceCommandError;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MyActivity";
-    private DiscoveryManager nDiscoveryManager;
-    private ConnectableDevice nDevice;
+public class MainActivity extends AppCompatActivity implements DiscoveryManagerListener {
 
     private ListView devicesListView;
-    private List<String> deviceNames = new ArrayList<>();
-    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> deviceNames;
+    private ArrayAdapter<String> devicesAdapter;
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initializing Discovery Manager
-        DiscoveryManager.init(getApplicationContext());
-        nDiscoveryManager = DiscoveryManager.getInstance();
-        nDiscoveryManager.start();
-
         devicesListView = findViewById(R.id.devicesListView);
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceNames);
-        devicesListView.setAdapter(arrayAdapter);
+        deviceNames = new ArrayList<>();
+        devicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, deviceNames);
+        devicesListView.setAdapter(devicesAdapter);
 
+        DiscoveryManager.init(this);
+        DiscoveryManager discoveryManager = DiscoveryManager.getInstance();
+        discoveryManager.addListener(this);
+        discoveryManager.start();
     }
 
-    void onDeviceAdded(DiscoveryManager manager, ConnectableDevice device) {
-        Log.d(TAG, "Device found: " + device.getFriendlyName());
+    @Override
+    public void onDeviceAdded(DiscoveryManager manager, ConnectableDevice device) {
+        Log.d("Discovery", "Device found: " + device.getFriendlyName());
         deviceNames.add(device.getFriendlyName());
-        arrayAdapter.notifyDataSetChanged();
+        devicesAdapter.notifyDataSetChanged();
     }
 
+    @Override
+    public void onDeviceUpdated(DiscoveryManager manager, ConnectableDevice device) {
+        Log.d("New:", "A device was updated");
+    }
+
+    @Override
     public void onDeviceRemoved(DiscoveryManager manager, ConnectableDevice device) {
-        Log.d(TAG, "Device lost: " + device.getFriendlyName());
+        Log.d("Disconnection.", "Device removed: " + device.getFriendlyName());
         deviceNames.remove(device.getFriendlyName());
-        arrayAdapter.notifyDataSetChanged();
+        devicesAdapter.notifyDataSetChanged();
     }
 
+    @Override
     public void onDiscoveryFailed(DiscoveryManager manager, ServiceCommandError error) {
-        Log.e(TAG, "Discovery failed: " + error.getMessage());
+        // Handle discovery failure
+        Log.e(TAG, "Hmm, an error has occurred somewhere");
     }
 }
